b5c5bd73cac8535676d514e2587b2d84dd762a54
Michael Velasquez <tgmike77@gmail.com>
2022-04-12 21:41:31 -0700

    Added Favorites Recipe Page design

---
+++ iEats/iEats/RecipeCell.swift
@@ -0,0 +1,23 @@
+//
+//  RecipeCell.swift
+//  iEats
+//
+//  Created by Michael Velasquez on 4/12/22.
+//
+
+import UIKit
+
+class RecipeCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}

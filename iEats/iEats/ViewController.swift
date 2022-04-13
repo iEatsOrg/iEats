8470043c03115043a0f8c5f5ae285f31fa91fd9f
Steven Schreiber <53790515+StevenRS00@users.noreply.github.com>
2022-04-12 20:19:44 -0700

    Empty Xcode project

---
+++ iEats/iEats/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iEats
+//
+//  Created by Steven Schreiber on 4/12/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

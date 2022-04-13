6cec7fb4b74b2165ee301400c1a613ffbbab43a4
Steven Schreiber <53790515+StevenRS00@users.noreply.github.com>
2022-04-12 21:16:28 -0700

    Added Recipe of the Day design

---
+++ iEats/iEats/RecipeViewController.swift
@@ -0,0 +1,29 @@
+//
+//  RecipeViewController.swift
+//  iEats
+//
+//  Created by Steven Schreiber on 4/12/22.
+//
+
+import UIKit
+
+class RecipeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
